8b03a162ba00aba63b186b798c36216d7e3a53bd
Leoltron <leoole5@gmail.com>
2017-04-01 15:18:07 +0500

    Enemy data collection added

--- AI.cs
+++ AI.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using CVARC.V2;
 using HoMM;
 using HoMM.ClientClasses;
 using ResourcePile = HoMM.ClientClasses.ResourcePile;
@@ -10,6 +12,8 @@ namespace Homm.Client
     {
         private HommClient client;
         private HommSensorData currentData;
+
+        private EnemyData enemyData;
 
         public AI(HommClient client, HommSensorData initialData)
         {
@@ -26,7 +30,15 @@ namespace Homm.Client
 
         private void UpdateData()
         {
-
+            var enemyUnitsData = new Dictionary<UnitType,int>();
+            foreach (var mapObject in currentData.Map.Objects)
+            {
+                //Не учитываю гарнизоны врага и врага, ибо хз как отличать от своих
+                if(mapObject.NeutralArmy != null)
+                    foreach (var army in mapObject.NeutralArmy.Army)
+                        enemyUnitsData.AddOrSum(army);
+            }
+            enemyData = new EnemyData(enemyUnitsData);
         }
 
         private Boolean wouldWinAttackAgainst(Dictionary<UnitType, int> enemy)
@@ -49,6 +61,23 @@ namespace Homm.Client
             currentData = data;// Не совсем увыерен, что тут еще что-то может быть, ну да ладно
         }
 
+        public struct EnemyData
+        {
+            private readonly Dictionary<UnitType, int> UnitAmount;
+            private readonly int amountOverall;
 
+            public double GetPart(UnitType type)
+            {
+                if (!UnitAmount.ContainsKey(type) || amountOverall == 0)
+                    return 0;
+                return (double) UnitAmount[type] / amountOverall;
+            }
+
+            public EnemyData(Dictionary<UnitType, int> unitAmount) : this()
+            {
+                UnitAmount = unitAmount;
+                amountOverall = UnitAmount.Sum(u => u.Value);
+            }
+        }
     }
 }
