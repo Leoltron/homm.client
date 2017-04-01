5eb3d36b90b324e42c1e6f4473254e0e4f610db9
Leoltron <leoole5@gmail.com>
2017-04-01 14:11:05 +0500

    Added skeleton

---
+++ AI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HoMM.ClientClasses;
+
+namespace Homm.Client
+{
+    public class AI
+    {
+        private HommSensorData Data;
+
+        public AI(HommSensorData data)
+        {
+            Data = data;
+        }
+
+        private double GetPileValue(ResourcePile pile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
