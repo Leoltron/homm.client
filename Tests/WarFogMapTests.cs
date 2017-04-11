753e61c797498954d637b354c5a445a092052900
Leoltron <leoole5@gmail.com>
2017-04-11 20:31:28 +0500

    Added WarFogMap tests

---
+++ Tests/WarFogMapTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+namespace Homm.Client.Tests
+{
+    [TestFixture]
+    [SuppressMessage("ReSharper", "UnusedVariable")]
+    internal class WarFogMapTests
+    {
+        [Test]
+        public void CreateTest()
+        {
+            var map = new WarFogMap();
+        }
+
+        [Test]
+        public void UpdateTest()
+        {
+            var map = new WarFogMap();
+            Assert.IsTrue(map.Map.Count == 0);
+            var mapProvider = DummyLocationMap.GetExampleMap();
+            map.UpdateMap(mapProvider);
+            foreach (var mapObject in mapProvider.Map.Objects)
+                Assert.IsTrue(map[mapObject.Location.ToLocation()] == mapObject);
+        }
+    }
+}
